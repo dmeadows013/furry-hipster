9f475b8f346a2953766d6c356f945049585ad118
Mike <mikec4891@gmail.com>
2012-09-01 14:18:54 -0700

    cpufreq: add LagFree Governor with fully working sysfs

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -372,6 +372,9 @@ extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAZY)
 extern struct cpufreq_governor cpufreq_gov_lazy;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lazy)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
+extern struct cpufreq_governor cpufreq_gov_lagfree;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lagfree)
 #endif
 
 
